ae68e648dcca6c263f3d16ad366ef0f715dd2bd5
Kyriakos Oikonomakos <kyriakos@hashicorp.com>
2018-10-23 17:50:46 +0300

    .

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ provisioner "local-exec" {
   command = "cat $HOME/.terraformrc"
 }
 
-module "nullresource" {
-  source  = "localterraform.com/test/nullresource/aws"
-  version = "0.0.1"
-}
-
-
-output "example" {
-  value = "${module.nullresource.example}"
-}
+#module "nullresource" {
+#  source  = "localterraform.com/test/nullresource/aws"
+#  version = "0.0.1"
+#}
+#
+#
+#output "example" {
+#  value = "${module.nullresource.example}"
+#}
