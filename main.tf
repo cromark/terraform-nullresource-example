f71a7f569fbf452937dcfb3def7ce48d92e173fd
Kyriakos Oikonomakos <kyriakos@hashicorp.com>
2019-01-25 20:46:51 +0200

    lol

--- main.tf
+++ main.tf
@@ -1,22 +1,26 @@
-data "local_file" "foo" {
-    filename = "/tmp/cli.tfrc"
-}
+#data "local_file" "foo" {
+#    filename = "/tmp/cli.tfrc"
+#}
+#
+#variable "lol" {
+#  description = "this should not be optional"
+#}
+#
+#
+#output "lol" {
+#  value = "${data.local_file.foo.content}"
+#}
+#
+#module "nullresource" {
+#  source  = "localterraform.com/test/nullresource/aws"
+#  version = "0.0.1"
+#}
+#
+#
+#output "example" {
+#  value = "${module.nullresource.example}"
+#}
 
-variable "lol" {
-  description = "this should not be optional"
-}
-
-
-output "lol" {
-  value = "${data.local_file.foo.content}"
-}
-
-module "nullresource" {
-  source  = "localterraform.com/test/nullresource/aws"
-  version = "0.0.1"
-}
-
-
-output "example" {
-  value = "${module.nullresource.example}"
+resource "random_pet" "console_password" {
+    length = 3
 }
